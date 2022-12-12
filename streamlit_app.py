d581e7c19809cbbe139057132051d047fc354fd8
smohammednasir <54725943+smohammednasir@users.noreply.github.com>
2022-12-11 18:20:43 -0600

    Update streamlit_app.py
    Added emojis and new title

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit
 
 streamlit.title('My Healthy Dinner Menu')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and blueberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Egg')
+streamlit.text('🥣 Omega 3 and blueberry oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-boiled Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
