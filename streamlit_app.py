80c5c6690685eff2b52445ab79f4fa3cdb4fcda3
smohammednasir <54725943+smohammednasir@users.noreply.github.com>
2022-12-11 18:19:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Healthy Dinner Menu')
 streamlit.header('Breakfast Menu')
-streamlit.text(':meal: Omega 3 and blueberry oatmeal')
+streamlit.text('Omega 3 and blueberry oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-boiled Egg')
