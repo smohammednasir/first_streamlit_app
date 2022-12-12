8e17ee7e8e872b30f4199fddea20ef3b19342745
smohammednasir <54725943+smohammednasir@users.noreply.github.com>
2022-12-11 18:27:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit
-import pandas
 
 streamlit.title('My Healthy Dinner Menu')
 streamlit.header('Breakfast Menu')
@@ -10,5 +9,5 @@ streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 
